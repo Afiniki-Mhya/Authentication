51a081b306de446120bbe91d12bcf7bfa49400aa
AFINIKI MHYA <johnafinikimhya12@gmail.com>
2024-06-23 20:19:45 +0100

    firebase auth

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,31 @@
-import React from "react";
+"use client";
+import React, { useState } from "react";
 import { BsEnvelope } from "react-icons/bs";
 import { FaFacebook, FaLinkedin } from "react-icons/fa";
 import { IoLogoTwitter } from "react-icons/io";
 import { FcGoogle } from "react-icons/fc";
 import { MdPassword } from "react-icons/md";
+import { useCreateUserWithEmailAndPassword } from "react-firebase-hooks/auth";
+import { auth } from "@/app/firebase/config";
 
-function Registerpage() {
+const Registerpage = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const [createUserWithEmailAndPassword] = useCreateUserWithEmailAndPassword(auth);
+
+  const handleRegisteration = async (e: { preventDefault: () => void; }) => {
+    e.preventDefault(); // Prevent form submission
+    try {
+      const res = await createUserWithEmailAndPassword(email, password);
+      console.log({ res });
+      setEmail("");
+      setPassword("");
+    } catch (e) {
+      console.error(e);
+    }
+  };
+
   return (
     <main className="relative bg-black min-h-screen">
       <div className="relative animate-pulse">
@@ -15,20 +35,23 @@ function Registerpage() {
           className="w-full h-screen object-cover"
         />
       </div>
-      
+
       {/* LOGO */}
-      <h1 className="absolute top-0 lg:left-0 lg:m-4 font-bold text-2xl p-3 text-white">Logo</h1>
-      
+      <h1 className="absolute top-0 lg:left-0 lg:m-4 font-bold text-2xl p-3 text-white">
+        Logo
+      </h1>
+
       {/* BOTTOM/RIGHTSIDE */}
       <div className="absolute inset-0 bg-black bg-opacity-80 flex justify-center items-center">
         <div className="w-full lg:w-1/3 md:w-1/2 h-auto bg-transparent bg-opacity-40 flex flex-col gap-6 rounded-lg text-white p-8">
-         <div className=" flex flex-col gap-3 text-center pb-5">
-           <h1 className="font-semibold text-2xl ">Hello</h1>
-           <p>Register and become a part of an exclusive club.
-            Fill in your required details and begin your journey.</p>
-         </div>
+          <div className=" flex flex-col gap-3 text-center pb-5">
+            <h1 className="font-semibold text-2xl ">Hello</h1>
+            <p>
+              Register and become a part of an exclusive club. Fill in your
+              required details and begin your journey.
+            </p>
+          </div>
           <div className="flex flex-col gap-6">
-            
             <form className="flex flex-col gap-4 items-center justify-center w-full">
               {/* EMAIL TEXT FIELD */}
               <div className="bg-[#d8dade] w-full p-1 flex flex-row items-center mb-1 rounded-md">
@@ -39,6 +62,8 @@ function Registerpage() {
                   className="outline-none bg-[#d8dade] text-sm rounded-sm w-full h-8 pl-2"
                   type="email"
                   name="email"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
                   placeholder="Email"
                 />
               </div>
@@ -50,6 +75,8 @@ function Registerpage() {
                   className="outline-none bg-[#d8dade] text-sm rounded-sm w-full h-8"
                   type="password"
                   name="password"
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
                   placeholder="Password"
                 ></input>
               </div>
@@ -61,7 +88,9 @@ function Registerpage() {
               </a>
               {/* SIGN IN BUTTON */}
               <div className="flex justify-center items-center w-full">
-                <button className="bg-blue-500 px-6 py-2 my-6 rounded-md text-white w-full">
+                <button 
+                onClick={handleRegisteration}
+                className="bg-blue-500 px-6 py-2 my-6 rounded-md text-white w-full">
                   Sign In
                 </button>
               </div>
@@ -71,25 +100,25 @@ function Registerpage() {
                 <hr className="flex-grow border-gray-400" />
               </div>
               <div className="flex flex-row gap-10 text-lg justify-center">
-              <a href="#">
-                <FaFacebook />
-              </a>
-              <a href="#">
-                <FaLinkedin />
-              </a>
-              <a href="#">
-                <IoLogoTwitter />
-              </a>
-              <a href="#">
-                <FcGoogle />
-              </a>
-            </div>
+                <a href="#">
+                  <FaFacebook />
+                </a>
+                <a href="#">
+                  <FaLinkedin />
+                </a>
+                <a href="#">
+                  <IoLogoTwitter />
+                </a>
+                <a href="#">
+                  <FcGoogle />
+                </a>
+              </div>
             </form>
           </div>
         </div>
       </div>
     </main>
   );
-}
+};
 
 export default Registerpage;
